7b22795675d4dfa18b897c9f0b580682a9e464eb
ibrahim erdogan <ibo9819@gmail.com>
2024-04-07 16:11:49 +0200

    third commit

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
-import(
-	"github.com/gorilla/mux"
+
+import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
 }
 type Movie struct {
-	Id       string    `json:"id"`
+	ID       string    `json:"id"`
 	ISBN     string    `json:"isbn"`
 	Title    string    `json:"title"`
 	Director *Director `json:"director"`
@@ -16,13 +21,16 @@ type Movie struct {
 
 var movies []Movie
 
-func main{
-	r:=mux.newRouter()
-	r.HandleFunc("/movies"getMovies.Methods("GET"))
-	r.HandleFunc("/movies{id}"getMovie.Methods("Get"))
-	r.HandleFunc("/movies"createMovie.Methods("POST"))
-	r.HandleFunc("/movies{id}"updateMovie.Methods("PUT"))
-	r.HandleFunc("/movies{id}"deleteMovie.Methods("Delete"))
-	
-
+func main() {
+	r := mux.NewRouter()
+	movies = append(movies, Movie{ID: "1", ISBN: "111", Title: "John Wick", Director: &Director{Firstname: "Chad", Lastname: "Stahlski"}})
+	movies = append(movies, Movie{ID: "2", ISBN: "112", Title: "Donnie Darko", Director: &Director{Firstname: "Richard", Lastname: "Kelly"}})
+	movies = append(movies, Movie{ID: "3", ISBN: "113", Title: "Scott Pilgrim vs The World", Director: &Director{Firstname: "Edgar", Lastname: "Wright"}})
+	r.HandleFunc("/movies", getMovies.Methods("GET"))
+	r.HandleFunc("/movies{id}", getMovie.Methods("Get"))
+	r.HandleFunc("/movies", createMovie.Methods("POST"))
+	r.HandleFunc("/movies{id}", updateMovie.Methods("PUT"))
+	r.HandleFunc("/movies{id}", deleteMovie.Methods("Delete"))
+	fmt.Printf("Starting server at port:8000")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
